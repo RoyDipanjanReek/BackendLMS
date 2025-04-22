df90d326e37968e86d694a7e2629fdd637342a6e
RoyDipanjanReek <dipanjanroy2003@gmail.com>
2025-04-22 18:05:14 +0530

    fix bugs

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -1,25 +1,22 @@
 export class ApiError extends Error {
-    constructor (message, statusCode) {
-        super(message);
-        this.statusCode = statusCode
-        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error'
-        this.isOperational = true //
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.isOperational = true; //
 
-
-        Error.captureStackTrace(this, this.constructor)
-    }
+    Error.captureStackTrace(this, this.constructor);
+  }
 }
-
 
 export const catchAsync = (fn) => {
-    return (req, res, next) => {
-        fn(req, res, next).catch(next)
-    }
-}
+  return (req, res, next) => {
+    fn(req, res, next).catch(next);
+  };
+};
 
 //handle jwt
 
 export const handleJWTError = () => {
-    new ApiError('Invalid token. please login again', 401)
-}
-
+  new ApiError("Invalid token. please login again", 401);
+};
